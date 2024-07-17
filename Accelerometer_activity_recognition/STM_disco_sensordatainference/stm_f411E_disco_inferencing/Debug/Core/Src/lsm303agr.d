8e4c1605df9fe7ff79d620d7aa1c3ca689aadbf9
Vishal Sivakumar <14985440+WiresharkIO@users.noreply.github.com>
2024-07-17 02:17:25 +0200

    Delete Accelerometer_activity_recognition/STM_disco_sensordatainference directory

--- Accelerometer_activity_recognition/STM_disco_sensordatainference/stm_f411E_disco_inferencing/Debug/Core/Src/lsm303agr.d
+++
@@ -1,4 +0,0 @@
-Core/Src/lsm303agr.o: ../Core/Src/lsm303agr.c ../Core/Inc/lsm303agr.h \
- ../Core/Inc/accelero.h
-../Core/Inc/lsm303agr.h:
-../Core/Inc/accelero.h:
--- Accelerometer_activity_recognition/STM_disco_sensordatainference/stm_f411E_disco_inferencing/Debug/Core/Src/lsm303dlhc.d
+++
@@ -1,4 +0,0 @@
-Core/Src/lsm303dlhc.o: ../Core/Src/lsm303dlhc.c ../Core/Inc/lsm303dlhc.h \
- ../Core/Inc/accelero.h
-../Core/Inc/lsm303dlhc.h:
-../Core/Inc/accelero.h:
